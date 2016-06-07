4e939043afa92ec41798830c5e5bb123d953b08c
AmitKrJoshi <AmitKrJoshi@users.noreply.github.com>
2016-06-07 23:28:31 +0530

    Checkin_1=>Design 2
    What is Design 2?
    Multiple Instance Design

--- App1/Source.cpp
+++ App1/Source.cpp
@@ -30,7 +30,7 @@ void mainCRTStartUp() // Stub Code
 /*--------------------------------------|
 |                                       |
 |************* DESIGN 1 ****************|
-|                                       |
+|       SINGLE INSTANCE DESIGN          |
 |--------------------------------------*/
 
 #if 0
@@ -113,5 +113,92 @@ This code is single stack code.
 How to make a code multi Stack ?
 
 */
+
+#endif
+
+/*--------------------------------------|
+|                                       |
+|************* DESIGN 2 ****************|
+|       MULTIPLE INSTANCE DESIGN        |
+|--------------------------------------*/
+/* There was issue with the design in Design 1. Can we fix it?
+Whenever we create global variable that is single instance program.
+When we want to go for Single stack go for global otherwise don't.
+
+How to make multiple instances of stack?
+[Ans]: Use struct
+*/
+
+#if 1
+/* Struct helps us to creates a new data type. No allocation happen. 
+ but if there is a global variable allocation will happen immediately before main.
+ For a data type there is no allocation.
+ When will allocation happen?
+ [Ans]:When we create variable of that Data type
+ */
+struct Stack // New Data Type (No allocation here)
+{
+	int items[100]; // global (single instance)
+	int top = 0; // global (single instance)
+};
+
+
+/* When we are creating this code we don't know whether there is going
+to be s1 or s2 or s3 [variables of Stack [objects]].. We don't know that.
+Hence I cannot write push which is specific to s1 or s2
+But we want push to work on anything whether s1 or s2
+Same for Pop
+How to achieve that?
+[Ans]: Accept there address. Design the function to accept the address of s1 or s2.
+Same thing for Pop.
+
+We have changed the Design. This is MULTIPLE INSTANCE DESIGN
+
+Multiple Instance Design => Don't make global variable. 
+                            Create Data type.
+                            Make global variable part of Data type.
+							Let multiple instance of data type to be created.
+							And design the function or push is such a way that it works on any instance of datatype
+
+*/
+void Push(Stack *s, int item)
+{
+	s->items[s->top] = item;
+	s->top++;
+}
+
+int Pop(Stack *s)
+{
+	s->top--;
+	return s->items[s->top];
+}
+
+//-----------------------------------------------------
+
+#include <stdio.h>
+void main()
+{
+	//Creating variable of data type Struct. This will do the allocation
+	// s1 and s2 are INSTANCES of data type
+	Stack s1, s2; // allocations
+	//Total bytes allocated => 
+	// for s1 => 100*4 + 4 = 404 bytes
+	// for s2 => 100*4 + 4 = 404 bytes
+	//Total => 808 bytes
+
+	Push(&s1, 100);
+	Push(&s1, 200);
+	Push(&s1, 300);
+
+	Push(&s2, 10000);
+	Push(&s2, 20000);
+	Push(&s2, 30000);
+
+	int item = Pop(&s1);
+	printf("%d\n", item);
+
+	item = Pop(&s2);
+	printf("%d\n", item);
+}
 
 #endif
