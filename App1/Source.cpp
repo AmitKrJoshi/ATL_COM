6c98e8c1bc3846d7976fffa474308c927fcfec97
AmitKrJoshi <AmitKrJoshi@users.noreply.github.com>
2016-06-07 20:08:02 +0530

    Checkin_3 -> Design - 1  [A]
    Starting of COM
    Design 1 - Why Pop 1 time and print 2 time

--- App1/Source.cpp
+++ App1/Source.cpp
@@ -10,19 +10,72 @@ but we never write that code
 [Ans.]That code is mainCRTStartUp() also called as stub code.
 */
 
-/* kernel32.dll - Windows OS
-mainCRTStartup() ;
+//>>>>>How main is called
+
+/* kernel32.dll - Windows OS calls
+	mainCRTStartup() ;
 */
 
 /* C++ Compiler will inject this code
 void mainCRTStartUp() // Stub Code
 {
-// Global Variables are created
-main();
+	//Before main gets called Global Variables are created
+	main();
 }
 */
 
+//>>>> Let's Start COM
 
+int items[100]; // global (single instance)
+int top = 0; // global (single instance)
+
+/*
+Push has accepted the value. What it will do after accepting the value? 
+It has to store it somewhere in such a way that it keeps track of the 
+Last one. So we'll create an array "items".
+*/
+
+/*
+We have made array item global. Anything global only gets created once. 
+We can call it single instance.
+
+Ok. So we'll store it in the array, but where in the array. So we’ll 
+create another variable and well call it “top” So
+top = 0
+that too global hence single instance.
+*/
+
+
+void Push(int item)
+{
+	items[top] = item;
+	top++;
+}
+/*
+Problem with Push => parameter fixed as int
+*/
+
+int Pop()
+{
+	top--;
+	return items[top];
+}
+/*
+Problem with Pop => return type is fixed as int
+*/
+
+//-----------------------------------------------------
+
+#include <stdio.h>
 void main()
 {
+	Push(100);	//Pushing 100
+	Push(200);	//Pushing 200
+	Push(300);
+
+	int item = Pop();
+	printf("%d\n", item);
+	printf("%d\n", item);
+	//Problem...?  
+	// we should call pop 2 or 3 times depending on how many value we want to see
 }
