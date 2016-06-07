bbc6993148ac6dfb0b461805d9ecd2fe1b4eec70
AmitKrJoshi <AmitKrJoshi@users.noreply.github.com>
2016-06-07 19:04:30 +0530

    Checkin_2
    Added some info not code

--- App1/Source.cpp
+++ App1/Source.cpp
@@ -1,7 +1,28 @@
 /*
 Void main () -> this will be marked as entry point of the application and the 
-main will be called by someone else. [Ques.]Now who calls the main?
+main will be called by someone else. 
+[Ques.]Now who calls the main?
+[Ans.]The exe code itself calls the main means your code itself calls the main 
+but we never write that code 
+[Ques.] who writes that code? 
+[Ans.]C++ Compiler will inject this code and that will call the main. 
+[Ques.] What is that code? 
+[Ans.]That code is mainCRTStartUp() also called as stub code.
 */
+
+/* kernel32.dll - Windows OS
+mainCRTStartup() ;
+*/
+
+/* C++ Compiler will inject this code
+void mainCRTStartUp() // Stub Code
+{
+// Global Variables are created
+main();
+}
+*/
+
+
 void main()
 {
 }
